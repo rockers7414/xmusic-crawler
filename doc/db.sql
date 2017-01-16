3fc3e67cc031e48232dacc4df5874931f9a4028e
RCK <rockers7414@gmail.com>
2017-01-17 00:46:31 +0800

    Add the docker file to setup the database environment.

---
+++ doc/db.sql
@@ -0,0 +1,98 @@
+CREATE EXTENSION pgcrypto;
+
+CREATE FUNCTION update_modtime() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+  BEGIN
+    NEW.updated_time = NOW();
+    RETURN NEW;
+  END;
+$$;
+
+CREATE TABLE IF NOT EXISTS genres (
+    genre_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(32) UNIQUE NOT NULL
+);
+
+CREATE UNIQUE INDEX idx_genres_name ON genres(name);
+
+CREATE TABLE IF NOT EXISTS datasources (
+    source_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(32) UNIQUE NOT NULL
+);
+
+CREATE UNIQUE INDEX idx_datasource_name ON datasource(name);
+
+CREATE TABLE IF NOT EXISTS artists (
+    artist_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    genre_id UUID REFERENCES genres(genre_id),
+    name VARCHAR(32) NOT NULL,
+    popularity BIGINT
+);
+
+CREATE INDEX idx_artists_name ON artists(name);
+CREATE INDEX idx_artists_popularity ON artists(popularity);
+
+CREATE TABLE IF NOT EXISTS albums (
+    album_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    artist_id UUID REFERENCES artists(artist_id),
+    name VARCHAR(32) NOT NULL,
+    popularity BIGINT
+);
+
+CREATE INDEX idx_albums_name ON albums(name);
+CREATE INDEX idx_albums_popularity ON albums(popularity);
+
+CREATE TABLE IF NOT EXISTS tracks (
+    track_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    album_id UUID REFERENCES albums(album_id),
+    name VARCHAR(32) NOT NULL,
+    popularity BIGINT,
+    track_number INT,
+    updated_time TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+CREATE TRIGGER update_tracks_modtime BEFORE UPDATE ON tracks FOR EACH ROW EXECUTE PROCEDURE update_modtime();
+CREATE INDEX idx_tracks_name ON tracks(name);
+CREATE INDEX idx_tracks_popularity ON tracks(popularity);
+
+CREATE TABLE IF NOT EXISTS repository (
+    track_id UUID REFERENCES tracks(track_id),
+    source_id UUID REFERENCES datasources(source_id),
+    link VARCHAR(512) UNIQUE NOT NULL,
+    duration_second INT
+);
+
+CREATE TABLE IF NOT EXISTS images (
+    image_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    width INT NOT NULL,
+    height INT NOT NULL
+);
+
+CREATE INDEX idx_images_name ON images(width);
+CREATE INDEX idx_images_popularity ON images(height);
+
+CREATE TABLE IF NOT EXISTS artists_images (
+    artist_id UUID REFERENCES artists(artist_id),
+    image_id UUID REFERENCES images(image_id)
+);
+
+CREATE TABLE IF NOT EXISTS albums_images (
+    album_id UUID REFERENCES albums(album_id),
+    image_id UUID REFERENCES images(image_id)
+);
+
+CREATE TABLE IF NOT EXISTS artists_genres (
+    artist_id UUID REFERENCES artists(artist_id),
+    genre_id UUID REFERENCES genres(genre_id)
+);
+
+CREATE TABLE IF NOT EXISTS albums_genres (
+    album_id UUID REFERENCES albums(album_id),
+    genre_id UUID REFERENCES genres(genre_id)
+);
+
+CREATE TABLE IF NOT EXISTS tracks_genres (
+    track_id UUID REFERENCES tracks(track_id),
+    genre_id UUID REFERENCES genres(genre_id)
+);
