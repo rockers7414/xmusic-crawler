a42fe6e7723729878518c36051771102642042f9
AbnerLin <shaoyang0805@gmail.com>
2017-02-08 18:21:09 +0800

    base64 update

--- daemon/authorization/spotify.py
+++ daemon/authorization/spotify.py
@@ -2,6 +2,7 @@ from urllib.parse import urlencode
 import requests
 import base64
 
+
 class Spotify:
 
     """
@@ -13,7 +14,7 @@ class Spotify:
     # Spotify Authorize API
     spotify_authorize_url = "https://accounts.spotify.com/api/token"
 
-    def __init__(self, client_id, client_secret, grant_type = "client_credentials"):
+    def __init__(self, client_id, client_secret, grant_type="client_credentials"):
         """
             Constructor
             Parameters:
@@ -22,31 +23,27 @@ class Spotify:
                 grant_type - grant type, set it to “client_credentials”. (Required)
         """
         self.parameter = {
-            grant_type : grant_type
+            grant_type: grant_type
         }
 
-        self.authorization = base64.b64encode(client_id + ":" + client_secret)
+        user_info_bytes = base64.b64encode((client_id + ":" + client_secret).encode())
+        self.user_info_base64 = user_info_bytes.decode()
 
         self.__authorize()
 
     def __authorize(self):
-        print(self.authorization)
+        headers = {"Authorization", "Basic " + self.user_info_base64}
 
-    def test(self):
+        pass
+
+    def display(self):
         print(self.spotify_authorize_url)
         print(self.spotify_authorize_url + "?" + urlencode(self.parameter))
-        self.__authorize()
-
-    def test2(self):
-        serial_param = urlencode(self.parameter)
-        print(serial_param)
-
-    def refresh_token(self):
-        pass
+        print(self.user_info_base64)
 
     def get_token(self):
         return "token.."
 
 
-a = Spotify("???", "???")
-a.test()
+a = Spotify("??", "??")
+a.display()
