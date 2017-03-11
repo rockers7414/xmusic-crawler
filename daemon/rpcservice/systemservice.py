ce73fe56375bef32a0997bdbe4ab305f232d605e
AbnerLin <shaoyang0805@gmail.com>
2017-03-12 00:18:24 +0800

    rename variable

--- daemon/rpcservice/systemservice.py
+++ daemon/rpcservice/systemservice.py
@@ -12,12 +12,12 @@ from decorator.singleton import singleton
 class SystemService(RPCService):
 
     def get_server_status(self):
-        cpu_status = {
+        system_status = {
             "cpu": psutil.cpu_percent(),
             "memory": psutil.virtual_memory().percent,
         }
         json_obj = []
-        json_obj.append(cpu_status)
+        json_obj.append(system_status)
         return json_obj
 
     def get_server_version(self):
