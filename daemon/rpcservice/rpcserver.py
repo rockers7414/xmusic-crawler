452936759149c7cf324219c4fd78afaeebee7e4d
RCK <rockers7414@gmail.com>
2017-03-19 22:03:06 +0800

    Add the client side code.

--- daemon/rpcservice/rpcserver.py
+++ daemon/rpcservice/rpcserver.py
@@ -3,12 +3,12 @@ import threading
 import socketserver
 import psutil
 
-from struct import pack, unpack
 from jsonrpc import JSONRPCResponseManager, dispatcher
 from enumtype.datasourcetype import DataSourceType
 from .dbservice import DBService
 from .spotifyservice import SpotifyService
 from .systemservice import SystemService
+from .messageheader import MessageHeader
 
 
 def service_factory(source):
@@ -36,7 +36,7 @@ def get_artists(index=1, offset=10, source=DataSourceType.DataBase.value):
 
 
 @dispatcher.add_method
-def get_artist(artist_name, source=DataSourceType.DataBase.value):
+def get_artist(artist_name=None, source=DataSourceType.DataBase.value):
     service = service_factory(source)
     result = service.get_artist(artist_name)
     return result
@@ -77,57 +77,37 @@ def get_server_status():
     return result
 
 
-class MessageHeader(object):
-
-    HEADER_LENGTH = 4
-    HEADER_SEPERATOR = 29
-
-    def __init__(self, data):
-        self.data_length = unpack("=BHB", data[:self.HEADER_LENGTH])[1]
-
-    @property
-    def length(self):
-        return self.data_length
-
-    @staticmethod
-    def create(data_length):
-        return pack("=BHB",
-                    MessageHeader.HEADER_SEPERATOR,
-                    data_length,
-                    MessageHeader.HEADER_SEPERATOR)
-
-
 class RPCHandler(socketserver.StreamRequestHandler):
     logger = logging.getLogger(__name__)
 
     MAX_BUF_LENGTH = 1024
 
     def handle(self):
-        self.logger.info("Handler thread name = {}/active count = {}"
-                         .format(threading.current_thread().name,
-                                 threading.active_count()))
-        header = MessageHeader(self.rfile.read(MessageHeader.HEADER_LENGTH))
-        self.logger.info("Message length = {}".format(header.length))
-        self.data = ""
-        while len(self.data) < header.length:
-            buf = self.rfile.read(
-                min(self.MAX_BUF_LENGTH, header.length - len(self.data)))
-            if not buf:
-                return None
-            self.data += str(buf, "UTF-8")
-            self.logger.info("current length = {}, total length = {}"
-                             .format(len(self.data),
-                                     header.length))
+        while True:
+            self.logger.info("Handler thread name = {}/active count = {}"
+                             .format(threading.current_thread().name,
+                                     threading.active_count()))
+            header = MessageHeader(
+                self.rfile.read(MessageHeader.HEADER_LENGTH))
+            self.logger.info("Message length = {}".format(header.length))
+            self.data = ""
+            while len(self.data) < header.length:
+                buf = self.rfile.read(
+                    min(self.MAX_BUF_LENGTH, header.length - len(self.data)))
+                self.data += str(buf, "UTF-8")
+                self.logger.info("current length = {}, total length = {}"
+                                 .format(len(self.data),
+                                         header.length))
 
-        self.logger.info("{0} request = {1}"
-                         .format(self.client_address[0],
-                                 self.data))
-        response = JSONRPCResponseManager.handle(self.data, dispatcher)
-        self.logger.info("response for {0} = {1}"
-                         .format(self.client_address[0],
-                                 response.json))
-        self.wfile.write(MessageHeader.create(
-            len(response.json)) + bytes(response.json, "UTF-8"))
+            self.logger.info("{0} request = {1}"
+                             .format(self.client_address[0],
+                                     self.data))
+            response = JSONRPCResponseManager.handle(self.data, dispatcher)
+            self.logger.info("response for {0} = {1}"
+                             .format(self.client_address[0],
+                                     response.json))
+            self.wfile.write(MessageHeader.create(
+                len(response.json)) + bytes(response.json, "UTF-8"))
 
 
 class RPCServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
